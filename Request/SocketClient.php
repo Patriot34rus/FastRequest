5075cfe5ec39e25e8ffab46373ebb005140d6f93
Кирилл Петров <Patriot34rus@yandex.ru>
2017-06-20 22:03:11 +0300

    добавил комментарии к SocketClient

--- Request/SocketClient.php
+++ Request/SocketClient.php
@@ -4,15 +4,38 @@ namespace Request;
 
 class SocketClient extends ARequest
 {
+    /**
+     * Singleton
+     * @var SocketClient
+     */
     private static $instance;
 
+    /**
+     * @var bool|resource
+     * @var int $buffer
+     */
     private $socket, $buffer = 4096;
 
+    /**
+     * SocketClient constructor.
+     * @param $URI
+     */
     private function __construct($URI)
     {
+        $errno =  $errstr = null;
         $this->socket = stream_socket_client($URI, $errno, $errstr);
+
+        if($errno){
+            throw new \Exception("Error Request. Socket - ".$errstr);
+        }
     }
 
+    /**
+     * Возвращает обеъкт текущего соединения или создает новое
+     * @param $URI
+     * @param array $options
+     * @return SocketClient
+     */
     public static function getInstance($URI, $options = [])
     {
         if (self::$instance === null) {
@@ -22,17 +45,27 @@ class SocketClient extends ARequest
         return self::$instance;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function exec()
     {
         fwrite($this->socket, $this->formatedComand());
         $this->response = fread($this->socket, $this->buffer);
     }
 
+    /**
+     * Форматирование данных в json для передачи в сокет
+     * @return string
+     */
     private function formatedComand()
     {
         return json_encode(['comand' => $this->comand, 'data' => $this->data]) . "\r\n";
     }
 
+    /**
+     * @inheritdoc
+     */
     public function close()
     {
         fclose($this->socket);
